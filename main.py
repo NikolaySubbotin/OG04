dc57c8d8be45ca22c93e8d546a351ad3133cb1f5
NikolaySubbotin <n.subbotin1986@gmail.com>
2024-06-18 00:02:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-
-for i in range(5):
-    print("hello")
-
-a, b = 7, 5
-
-if a > b:
-    print("a больше b")
-elif a < b:
-    print("a меньше b")
-else:
-    print("a равно b")
-
+print("hello")
 
 
